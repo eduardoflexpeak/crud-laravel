f8e852a98edadcecaf2e57a43cba425a6dee0806
Eduardo Silva <eduardo@dudstecnologia.com.br>
2020-08-01 16:08:14 -0400

    Navegação Pessoas

---
+++ resources/views/pessoa/create.blade.php
@@ -0,0 +1,12 @@
+<h1>Formulário de Pessoa</h1>
+
+<form action="/pessoas" method="post">
+    <input type="text" name="nome">
+    <input type="text" name="telefone">
+    <input type="text" name="email">
+    <button type="submit">Salvar</button>
+</form>
+
+<br>
+
+<a href="/pessoas">Voltar</a>
