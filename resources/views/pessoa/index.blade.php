62fd908bad69b1a6938bea0338254f1f45082e3f
Eduardo Silva <eduardo@dudstecnologia.com.br>
2020-08-22 14:40:53 -0400

    Template inicio

--- resources/views/pessoa/index.blade.php
+++ resources/views/pessoa/index.blade.php
@@ -1,5 +1,8 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+@extends('template.master')
 
+@section('titulo', 'Lista de Pessoas')
+
+@section('conteudo')
 <h1>Pessoas Cadastradas</h1>
 
 <a class="btn btn-primary" href="/pessoas/create">Cadastrar Nova</a>
@@ -21,12 +24,8 @@
         <td>
             <a href="/pessoas/{{ $p->id }}/edit">Editar</a>
             <a href="/pessoas/{{ $p->id }}">Excluir</a>
-            {{-- <form action="/pessoas/{{ $p->id }}" method="post">
-                @csrf
-                @method('DELETE')
-                <input type="submit" value="Excluir">
-            </form> --}}
         </td>
     </tr>
     @endforeach
 </table>
+@endsection
